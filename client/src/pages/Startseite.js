b3105faada86968634d606286b5da9622332f9a0
unknown <daniel-nedic@hotmail.de>
2025-07-21 10:55:40 +0200

    gestureHandling activated

--- client/src/pages/Startseite.js
+++ client/src/pages/Startseite.js
@@ -94,12 +94,13 @@ function Startseite() {
             <div className="home-section bottom-left">
                 <iframe
                     title="Google Maps Gundelsheim"
-                    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d650.6341572740376!2d9.157402269658187!3d49.28517687300742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDnCsDE3JzA2LjYiTiA5wrAwOScyOS4wIkU!5e0!3m2!1sde!2sde!4v1736963839363!5m2!1sde!2sde"
+                    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d650.6341572740376!2d9.157402269658187!3d49.28517687300742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDnCsDE3JzA2LjYiTiA5wrAwOScyOS4wIkU!5e0!3m2!1sde!2sde!4v1736963839363!5m2!1sde!2sde&gestureHandling=cooperative"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
                     allowFullScreen=""
                     loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
             </div>
 
